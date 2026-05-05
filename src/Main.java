e1ab34d22737e86baed2f62e6a07be2bdb269f9e
Nuraslanbek Yessentur <nuraslanbek@MacBook-Pro-Nuraslanbek.local>
2026-05-05 22:43:05 +0500

    move java files into src structure

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Main {
+
+    /**
+     * Default constructor
+     */
+    public Main() {
+    }
+
+
+    /**
+     * @param args 
+     * @return
+     */
+    public static void main(Set<String> args) {
+        // TODO implement here
+        return null;
+    }
+
+}
