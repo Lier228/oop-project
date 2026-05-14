776e1822f7e7c7b90a6f41d6a5da660eb522c8d0
Yernar111 <ernarsuinullaev1@gmail.com>
2026-05-14 19:06:42 +0500

    updated addUser() method in Admin class

--- src/main/java/oopproject/users/Admin.java
+++ src/main/java/oopproject/users/Admin.java
@@ -1,12 +1,14 @@
 package oopproject.users;
 
 import java.util.Scanner;
+import oopproject.core.University;
 import oopproject.enums.UserType;
 
 public class Admin extends User {
-    private final Scanner scanner = new Scanner(System.in);
+    private final Scanner sc = new Scanner(System.in);
 
     public Admin() {
+
     }
 
     public Admin(int id, String username, String password, String email) {
@@ -14,6 +16,25 @@ public class Admin extends User {
     }
 
     public void addUser() {
+        System.out.print("Username: ");
+        String username = sc.nextLine();
+
+        System.out.print("Password: ");
+        String password = sc.nextLine();
+
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+
+        System.out.println("Type: 1-Admin 2-Student 3-Teacher 4-Manager");
+        int typeChoice = Integer.parseInt(sc.nextLine());
+        UserType type = UserType.STUDENT;
+        if(typeChoice == 1) type = UserType.ADMIN;
+        if(typeChoice == 3) type = UserType.TEACHER;
+        if(typeChoice == 4) type = UserType.MANAGER;
+
+        User user = UserFactory.createUser(type, 100 + University.getInstance().users.size(), username, password, email);
+        University.getInstance().users.add(user);
+        System.out.println("User added: " + user.username);
     }
 
     public void removeUser() {
@@ -28,3 +49,5 @@ public class Admin extends User {
     public void assignRole(User user, UserType role) {
     }
 }
+
+//
