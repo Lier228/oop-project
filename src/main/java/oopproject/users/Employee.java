b7eb5f7977d8ddfbc40c972b9cbe8cb6abcb6dc2
Yernar111 <ernarsuinullaev1@gmail.com>
2026-05-17 21:53:39 +0500

    updated employee

--- src/main/java/oopproject/users/Employee.java
+++ src/main/java/oopproject/users/Employee.java
@@ -5,19 +5,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import oopproject.academic.Request;
+import oopproject.enums.RequestStatus;
+import oopproject.enums.RequestType;
 
 public class Employee extends User {
     protected double salary;
     protected LocalDate hireDate;
+    protected boolean active=false;
+    protected String department;
     protected List<Request> requests = new ArrayList<>();
 
     public Employee() {
     }
 
-    public Employee(int id, String username, String password, String email, double salary, LocalDate hireDate) {
+    public Employee(int id, String username, String password, String email, double salary, LocalDate hireDate, String department) {
         super(id, username, password, email);
         this.salary = salary;
         this.hireDate = hireDate;
+        this.department = department;
     }
 
     public List<Request> getRequestInstance() {
@@ -34,5 +39,14 @@ public class Employee extends User {
 
     public void addRequest(Request request) {
         requests.add(request);
+    }
+
+    public void changeActive() {
+        if (active) {
+            this.active=false;
+        }
+        else{
+            this.active=true;
+        }
     }
 }
